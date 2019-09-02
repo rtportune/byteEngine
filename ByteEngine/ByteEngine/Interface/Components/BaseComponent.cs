2e68c4ff45abdcc7a18666bf4201f6293bf5745a
rportune <richard.portune@thermofisher.com>
2019-09-02 16:33:43 -0700

    Initial folder and project Setup

---
+++ ByteEngine/ByteEngine/Interface/Components/BaseComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteEngine.Interface.Components
+{
+    public class BaseComponent : IComponent
+    {
+        #region IComponent Implementation
+        public uint ID { get; set; }
+        public uint EntityID { get; set; }
+        #endregion
+    }
+}
---
+++ ByteEngine/ByteEngine/Interface/IComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteEngine.Interface
+{
+    public interface IComponent
+    {
+        /// <summary>
+        /// Gets or sets the ID of this Component
+        /// </summary>
+        uint ID { get; set; }
+        /// <summary>
+        /// Gets or sets the ID of the Entity that this Component is a part of
+        /// </summary>
+        uint EntityID { get; set; }
+    }
+}
